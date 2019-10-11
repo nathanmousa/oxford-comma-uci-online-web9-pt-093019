8e4800a597627277b422378d8023ce80105de471
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-11 16:58:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  end
+  
+  if array.count == 2
+    array.join(" and ")
+  end
+  
+  if array.count == 3
+    array.join
+  end
 end
